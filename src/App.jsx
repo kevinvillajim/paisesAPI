737592dbc90526bfaecaac7b87c298ec797ad1cb
kevinvillajim <kevinvillajim@hotmail.com>
2023-10-12 15:39:59 -0500

    Cambios mayores

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,78 @@
-import { useEffect, useState } from 'react'
-import Container from './Container';
-import './App.css'
+import { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
+import Cards from './components/Cards';
+import './App.css';
 
 function useData() {
   const [data, setData] = useState(null);
   useEffect(() => {
     const getData = async () => {
-      const res = await fetch(
-        "https://restcountries.com/v3.1/all"
-      )
-      const datos = await res.json();
-      setData(datos);
+      try {
+        const res = await fetch("https://restcountries.com/v3.1/all");
+                const datos = await res.json();
+        setData(datos);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
+      }
     };
     getData();
   }, []);
-  return { data };
+  const saludar = () => alert("Hola Mundo");
+  return { data, saludar };
 }
 
 function App() {
-  const { data, saludar } = useData();
-  console.log(data);
-  console.log(saludar);
+  const { data} = useData();
+  const [searchTerm, setSearchTerm] = useState('');
+  const [filteredCountries, setFilteredCountries] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      setFilteredCountries(data);
+    }
+  }, [data]);
+
+  const handleSearch = (e) => {
+    const term = e.target.value;
+    setSearchTerm(term);
+    const filteredResults = data ? data.filter((country) => {
+      return country.name.official.toLowerCase().includes(term.toLowerCase())
+    }) : [];
+    setFilteredCountries(filteredResults);
+  };
   
-  return <>
-      <Container/>
-  </>
+  return (
+    <>
+      <div className="totalContainer">
+        <header className="header">
+          <h2>Where in the world?</h2>
+          <button className="darkMode"><span className="material-symbols-outlined">dark_mode</span>Dark Mode</button>
+        </header>
+        <div className="bodyContainer">
+          <div className="filtersContainer">
+            <div className="search-container">
+              <input className="search-input" type="text" placeholder="Search for a country..." value={searchTerm} onChange={handleSearch}/>
+              <span className="material-symbols-outlined search-icon">search</span>
+            </div>
+            <h2>Desplegable</h2>
+          </div>
+          <div className="countriesContainer">
+            {filteredCountries.map((country, index) => (
+              <Cards key={index} country={country} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
+
+App.propTypes = {
+  data: PropTypes.array,
+};
+
+App.defaultProps = {
+  data: [],
+};
--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,34 +4,30 @@ function Cards({ country }) {
   return (
     <>
       <main>
-        {country.map((el, index) => {
-          return (
-            <div className="car" key={index}>
-              <div className="DivImg">
-                <img src={el.flags.png} alt={el.name.official}></img>
-              </div>
-              <div className="DivText">
-                <h3 className="tit">
-                  <strong>{el.name.official}</strong>
-                </h3>
-                <ul>
-                  <li>Population: {el.population}</li>
-                  <br />
-                  <li>Region: {el.region}</li>
-                  <br />
-                  <li>Capital: {el.capital}</li>
-                </ul>
-              </div>
-            </div>
-          );
-        })}
+        <div className="car">
+          <div className="DivImg">
+            <img src={country.flags.png} alt={country.name.official}></img>
+          </div>
+          <div className="DivText">
+            <h3 className="tit">
+              <strong>{country.name.official}</strong>
+            </h3>
+            <ul>
+              <li>Population: {country.population}</li>
+              <br />
+              <li>Region: {country.region}</li>
+              <br />
+              <li>Capital: {country.capital}</li>
+            </ul>
+          </div>
+        </div>
       </main>
     </>
   );
 }
 
 Cards.propTypes = {
-  country: PropTypes.arrayOf(PropTypes.object).isRequired,
+  country: PropTypes.object.isRequired,
 };
 
 export default Cards;
