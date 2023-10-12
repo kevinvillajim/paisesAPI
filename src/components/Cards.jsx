a3b105e73fdea5198722c49bf0bb33f8ea837681
kevinvillajim <kevinvillajim@hotmail.com>
2023-10-12 15:55:41 -0500

    Resuelve conflictos de fusión

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 function Cards({ country }) {
   return (
     <>
-<<<<<<< HEAD
       <main>
         <div className="car">
           <div className="DivImg">
@@ -22,26 +21,6 @@ function Cards({ country }) {
             </ul>
           </div>
         </div>
-=======
-      <main>ñ
-        <div className="car">
-          <div className="DivImg">
-            <img src={country.flags.png} alt={country.name.official}></img>
-          </div>
-          <div className="DivText">
-            <h3 className="tit">
-              <strong>{country.name.official}</strong>
-            </h3>
-            <ul>
-              <li>Population: {country.population}</li>
-              <br />
-              <li>Region: {country.region}</li>
-              <br />
-              <li>Capital: {country.capital}</li>
-            </ul>
-          </div>
-        </div>
->>>>>>> 9febef174dfbd8f1ba1c4757e8a14afbe87f508c
       </main>
     </>
   );
