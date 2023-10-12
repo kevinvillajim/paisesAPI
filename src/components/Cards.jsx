efd31f8947c94abcc0b10e858681f4f498485293
kevinvillajim <kevinvillajim@hotmail.com>
2023-10-12 15:49:04 -0500

    Resuelve conflictos de fusión

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -23,28 +23,24 @@ function Cards({ country }) {
           </div>
         </div>
 =======
-      <main className="container">
-        {data.map((el, index) => {
-          return (
-            <div className="car" key={index}>
-              <div className="DivImg">
-                <img src={el.flags.png} alt={el.name.official}></img>
-              </div>
-              <div className="DivText">
-                <h3 className="tit">
-                  <strong>{el.name.official}</strong>
-                </h3>
-                <ul>
-                  <li>Population: {el.population}</li>
-                  <br />
-                  <li>Region: {el.region}</li>
-                  <br />
-                  <li>Capital: {el.capital}</li>
-                </ul>
-              </div>
-            </div>
-          );
-        })}
+      <main>ñ
+        <div className="car">
+          <div className="DivImg">
+            <img src={country.flags.png} alt={country.name.official}></img>
+          </div>
+          <div className="DivText">
+            <h3 className="tit">
+              <strong>{country.name.official}</strong>
+            </h3>
+            <ul>
+              <li>Population: {country.population}</li>
+              <br />
+              <li>Region: {country.region}</li>
+              <br />
+              <li>Capital: {country.capital}</li>
+            </ul>
+          </div>
+        </div>
 >>>>>>> 9febef174dfbd8f1ba1c4757e8a14afbe87f508c
       </main>
     </>
